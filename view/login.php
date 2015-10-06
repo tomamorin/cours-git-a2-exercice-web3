a6d208b978f4bce588b2d8b19fef7e2fe4362a56
Sacha Bobinet <sachabobinet@gmail.com>
2015-10-06 17:39:39 +0200

    Ajouts des balises name

--- view/login.php
+++ view/login.php
@@ -12,16 +12,16 @@
 				<form method="POST" action="login.php">
 					<div class="form-input">
 						<span class="email">
-							<input type="text" placeholder="Email">
+							<input name="nom" type="text" name="email" placeholder="Email">
 						</span>
 					</div>
 					<div class="form-input">
 						<span class="password">
-							<input type="password" placeholder="Password">
+							<input name="nom" type="password" name="password" placeholder="Password">
 						</span>
 					</div>
 					<div class="form-submit">
-						<input type="submit" value="Connecter">	
+						<input name="nom" type="submit" value="Connecter">	
 					</div>
 					<p class="account">Vous n'avez pas de compte ? <a href="register.php">Inscrivez vous !</a></p>
 				</form>
